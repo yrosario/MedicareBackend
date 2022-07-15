efded3c59271602e559bd2d0c3b0572f3d04d417
Yussel Rosario <yusselrosario@gmail.com>
2022-07-15 14:35:29 -0400

    Cleanup. Removed unused dependency from product entity class

--- src/main/java/com/medicare/backend/model/Product.java
+++ src/main/java/com/medicare/backend/model/Product.java
@@ -3,7 +3,6 @@
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;

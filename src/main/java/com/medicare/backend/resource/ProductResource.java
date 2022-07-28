dbbcd8406e6a90cfa28ce6f242a7c4207bd9338b
Yussel Rosario <yusselrosario@gmail.com>
2022-07-27 20:29:42 -0400

    Feature. Added Get method to retrieve image from the product resource.

--- src/main/java/com/medicare/backend/resource/ProductResource.java
+++ src/main/java/com/medicare/backend/resource/ProductResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -127,6 +128,17 @@ public class ProductResource {
 		}catch(Exception e) {
 			return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	@GetMapping("{id}/image")
+	public ResponseEntity<?> getImage(@PathVariable("id") Long id){
+		Product product = productService.findById(id);
+		if(product == null) {
+			return new ResponseEntity<>("Product:Not_Found", HttpStatus.NOT_FOUND);
+		}
+		
+		byte[] encodeImg = product.getImageBlob();
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(encodeImg);
 	}
 
 }
